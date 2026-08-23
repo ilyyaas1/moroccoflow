c560013c85d53ff40e408e59f4c70641b20607b5
ilyyaas1 <ilyassfar380@gmail.com>
2026-08-23 18:26:57 +0100

    start

---
+++ src/main/java/org/services/moroccoflow/MoroccoflowApplication.java
@@ -0,0 +1,13 @@
+package org.services.moroccoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoroccoflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoroccoflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/services/moroccoflow/MoroccoflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.services.moroccoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroccoflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
