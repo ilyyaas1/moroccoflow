9eead88c3bce5dcdd0b98b015ad257e1f5bca6b2
ilyyaas1 <ilyassfar380@gmail.com>
2026-08-23 19:19:50 +0100

    feat: postgres via docker compose, env-based config, flyway + swagger + actuator

--- src/main/java/com/moroccoflow/MoroccoflowApplication.java
+++ src/main/java/com/moroccoflow/MoroccoflowApplication.java
@@ -1,4 +1,4 @@
-package org.services.moroccoflow;
+package com.moroccoflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/moroccoflow/MoroccoflowApplicationTests.java
+++ src/test/java/com/moroccoflow/MoroccoflowApplicationTests.java
@@ -1,4 +1,4 @@
-package org.services.moroccoflow;
+package com.moroccoflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
